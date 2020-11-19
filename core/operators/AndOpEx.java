8b8356b7f30e49f8bb380a58b4edbb7c3ae4ebe0
haindavi-tokala <74723991+haindavi-tokala@users.noreply.github.com>
2020-11-19 20:32:26 +0530

    Create AndOpEx.java

---
+++ core/operators/AndOpEx.java
@@ -0,0 +1,11 @@
+class AndOpEx
+    {
+       public static void main(String args[])
+       {
+       int a=2;
+       int b=4;
+       int c=0;
+       c=a&b;
+       System.out.println("ans is "+c);
+       }
+    }   
